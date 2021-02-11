2264c0872879671fb955e0ffe8d79a749cd7754b
patrickrnelson <patrickrnelson1@gmail.com>
2021-02-10 20:55:14 -0600

    whoops. forgot to commit earlier. just finished the PUT on books

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
   console.log('jQuery sourced.');
   refreshBooks();
   addClickHandlers();
@@ -8,6 +8,8 @@ function addClickHandlers() {
   $('#submitBtn').on('click', handleSubmit);
 
   // TODO - Add code for edit & delete buttons
+  $(document).on('click', '#statusBtn', onStatusClick);
+  $(document).on('click', '#deleteBtn', onDeleteClick);
 }
 
 function handleSubmit() {
@@ -24,11 +26,13 @@ function addBook(bookToAdd) {
     type: 'POST',
     url: '/books',
     data: bookToAdd,
-    }).then(function(response) {
+  })
+    .then(function (response) {
       console.log('Response from server.', response);
       refreshBooks();
-    }).catch(function(error) {
-      console.log('Error in POST', error)
+    })
+    .catch(function (error) {
+      console.log('Error in POST', error);
       alert('Unable to add book at this time. Please try again later.');
     });
 }
@@ -37,28 +41,66 @@ function addBook(bookToAdd) {
 function refreshBooks() {
   $.ajax({
     type: 'GET',
-    url: '/books'
-  }).then(function(response) {
-    console.log(response);
-    renderBooks(response);
-  }).catch(function(error){
-    console.log('error in GET', error);
-  });
+    url: '/books',
+  })
+    .then(function (response) {
+      console.log(response);
+      renderBooks(response);
+    })
+    .catch(function (error) {
+      console.log('error in GET', error);
+    });
 }
-
 
 // Displays an array of books to the DOM
 function renderBooks(books) {
   $('#bookShelf').empty();
 
-  for(let i = 0; i < books.length; i += 1) {
+  for (let i = 0; i < books.length; i += 1) {
     let book = books[i];
     // For each book, append a new row to our table
     $('#bookShelf').append(`
       <tr>
         <td>${book.title}</td>
         <td>${book.author}</td>
+        <td class="isReadStatus">${book.isRead}</td>
+        <td><button id="statusBtn" data-id="${book.id}">Change Status</button></td>
+        <td><button id="deleteBtn" data-id="${book.id}">DELETE</button></td>
       </tr>
     `);
   }
+}
+
+// Edit book status
+// we will call this funciton inside of the click handler function
+// in order to pass the book ID and statusChange
+function changeReadStatus(bookID, currentStatus) {
+  $.ajax({
+    method: 'PUT',
+    // need the book ID in the url
+    // can grab with data-id
+    // will get with the click handler
+    url: `/books/isRead/${bookID}`,
+    data: {
+      currentStatus: currentStatus,
+    },
+  })
+    .then(function (response) {
+      refreshBooks();
+    })
+    .catch(function (err) {
+      console.log('error', err);
+      alert('Error! Try again later');
+    });
+}
+
+function onStatusClick() {
+  console.log('Status Click');
+  let thisBookID = $(this).data('id');
+  let currentStatus = $(this).parent().siblings('.isReadStatus').text();
+  changeReadStatus(thisBookID, currentStatus);
+}
+
+function onDeleteClick() {
+  console.log('Delete Click');
 }
--- server/routes/book.router.js
+++ server/routes/book.router.js
@@ -6,29 +6,32 @@ const pool = require('../modules/pool');
 // Get all books
 router.get('/', (req, res) => {
   let queryText = 'SELECT * FROM "books" ORDER BY "title";';
-  pool.query(queryText).then(result => {
-    // Sends back the results in an object
-    res.send(result.rows);
-  })
-  .catch(error => {
-    console.log('error getting books', error);
-    res.sendStatus(500);
-  });
+  pool
+    .query(queryText)
+    .then((result) => {
+      // Sends back the results in an object
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log('error getting books', error);
+      res.sendStatus(500);
+    });
 });
 
 // Adds a new book to the list of awesome reads
 // Request body must be a book object with a title and author.
-router.post('/',  (req, res) => {
+router.post('/', (req, res) => {
   let newBook = req.body;
   console.log(`Adding book`, newBook);
 
   let queryText = `INSERT INTO "books" ("author", "title")
                    VALUES ($1, $2);`;
-  pool.query(queryText, [newBook.author, newBook.title])
-    .then(result => {
+  pool
+    .query(queryText, [newBook.author, newBook.title])
+    .then((result) => {
       res.sendStatus(201);
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(`Error adding new book`, error);
       res.sendStatus(500);
     });
@@ -38,11 +41,46 @@ router.post('/',  (req, res) => {
 // Updates a book to show that it has been read
 // Request must include a parameter indicating what book to update - the id
 // Request body must include the content to update - the status
+router.put('/isRead/:id', (req, res) => {
+  console.log('req.body', req.body);
+  console.log('req.params', req.params);
+  let bookID = req.params.id;
 
+  // Current Status will come from the request body
+  // Expected to be true or false
 
-// TODO - DELETE 
+  let currentStatus = req.body.currentStatus;
+  let sqlText = '';
+
+  if (currentStatus === 'false') {
+    // if we receive a currentStatus that = 'false'
+    // set the isRead column to True
+    sqlText = `UPDATE books SET "isRead"=TRUE WHERE id=$1`;
+    // else if we receive a statusChange that = 'unread'
+    // set the isRead column to False
+  } else if (currentStatus === 'true') {
+    sqlText = `UPDATE books SET "isRead"=FALSE WHERE id=$1`;
+  } else {
+    // If we don't get an expected direction, send back bad status
+    console.log('Whoops');
+    res.sendStatus(500);
+    return; // Do it now, doesn't run the next set of code
+  }
+
+  pool
+    .query(sqlText, [bookID])
+    .then((dbRes) => {
+      console.log(dbRes);
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log('error', err);
+      res.sendStatus(500);
+    });
+});
+
+// TODO - DELETE
 // Removes a book to show that it has been read
 // Request must include a parameter indicating what book to update - the id
-
 
 module.exports = router;
